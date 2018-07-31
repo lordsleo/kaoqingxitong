0a63400255f15f0be1f48e94406e22fe105b044d
shenhgithub <shenh2013@163.com>
2018-07-31 15:13:00 +0800

    Signed-off-by: shenhgithub <shenh2013@163.com>

---
+++ 考勤系统/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace 考勤系统
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btResure = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Txt_user = new System.Windows.Forms.TextBox();
+            this.txt_Pwd = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.dc_Company = new System.Windows.Forms.ComboBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancel.ForeColor = System.Drawing.Color.Black;
+            this.btCancel.Location = new System.Drawing.Point(358, 212);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(65, 24);
+            this.btCancel.TabIndex = 22;
+            this.btCancel.Text = "退  出";
+            this.btCancel.UseVisualStyleBackColor = false;
+            // 
+            // btResure
+            // 
+            this.btResure.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btResure.BackColor = System.Drawing.Color.Transparent;
+            this.btResure.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btResure.ForeColor = System.Drawing.Color.Black;
+            this.btResure.Location = new System.Drawing.Point(268, 212);
+            this.btResure.Name = "btResure";
+            this.btResure.Size = new System.Drawing.Size(65, 24);
+            this.btResure.TabIndex = 21;
+            this.btResure.Text = "登 录";
+            this.btResure.UseVisualStyleBackColor = false;
+            // 
+            // Label2
+            // 
+            this.Label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label2.BackColor = System.Drawing.Color.Transparent;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.ForeColor = System.Drawing.Color.Black;
+            this.Label2.Location = new System.Drawing.Point(227, 137);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(80, 20);
+            this.Label2.TabIndex = 24;
+            this.Label2.Text = "用  户:";
+            this.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label1
+            // 
+            this.Label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label1.BackColor = System.Drawing.Color.Transparent;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.ForeColor = System.Drawing.Color.Black;
+            this.Label1.Location = new System.Drawing.Point(227, 113);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(80, 20);
+            this.Label1.TabIndex = 23;
+            this.Label1.Text = "部  门:";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Txt_user
+            // 
+            this.Txt_user.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Txt_user.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_user.Location = new System.Drawing.Point(307, 137);
+            this.Txt_user.Name = "Txt_user";
+            this.Txt_user.Size = new System.Drawing.Size(144, 21);
+            this.Txt_user.TabIndex = 19;
+            // 
+            // txt_Pwd
+            // 
+            this.txt_Pwd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_Pwd.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Pwd.Location = new System.Drawing.Point(307, 161);
+            this.txt_Pwd.Name = "txt_Pwd";
+            this.txt_Pwd.PasswordChar = '*';
+            this.txt_Pwd.Size = new System.Drawing.Size(144, 21);
+            this.txt_Pwd.TabIndex = 20;
+            // 
+            // Label3
+            // 
+            this.Label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label3.BackColor = System.Drawing.Color.Transparent;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.ForeColor = System.Drawing.Color.Black;
+            this.Label3.Location = new System.Drawing.Point(235, 161);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(64, 20);
+            this.Label3.TabIndex = 25;
+            this.Label3.Text = "密  码:";
+            this.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dc_Company
+            // 
+            this.dc_Company.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dc_Company.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dc_Company.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.dc_Company.Location = new System.Drawing.Point(307, 113);
+            this.dc_Company.Name = "dc_Company";
+            this.dc_Company.Size = new System.Drawing.Size(144, 22);
+            this.dc_Company.TabIndex = 18;
+            // 
+            // Label4
+            // 
+            this.Label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Label4.BackColor = System.Drawing.Color.Transparent;
+            this.Label4.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.ForeColor = System.Drawing.Color.Transparent;
+            this.Label4.Location = new System.Drawing.Point(102, 256);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(216, 21);
+            this.Label4.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 401);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btResure);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Txt_user);
+            this.Controls.Add(this.txt_Pwd);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.dc_Company);
+            this.Controls.Add(this.Label4);
+            this.Name = "Form1";
+            this.Text = "连云港外理_考勤系统";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btCancel;
+        internal System.Windows.Forms.Button btResure;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox Txt_user;
+        internal System.Windows.Forms.TextBox txt_Pwd;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.ComboBox dc_Company;
+        internal System.Windows.Forms.Timer Timer1;
+        internal System.Windows.Forms.Label Label4;
+    }
+}
+
